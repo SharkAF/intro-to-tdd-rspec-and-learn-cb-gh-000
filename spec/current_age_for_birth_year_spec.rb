ca10cc142586237fe467dfd6544aa95a69411461
Daniel Eitani <daniel.eitani@dawsoncollege.qc.ca>
2020-02-11 06:36:17 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -3,7 +3,7 @@ require_relative '../current_age_for_birth_year.rb'
 describe "current_age_for_birth_year method" do
   it "should return the current year for a person born in year 0" do
     age = current_age_for_birth_year(0)
-   
+
     expect(age).to eq(2003)
   end
 end
