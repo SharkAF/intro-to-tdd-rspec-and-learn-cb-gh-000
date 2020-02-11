099aa573e02dc625b5946aa96266ff3c05fa6b26
Daniel Eitani <daniel.eitani@dawsoncollege.qc.ca>
2020-02-11 06:27:11 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -2,5 +2,5 @@ def current_age_for_birth_year(x)
   it "should return the current year for a person born in year 0" do
   age = current_age_for_birth_year(0)
 
-  expect(age).to eq(2003)
+  expect(age).to eq(2020)
 end
